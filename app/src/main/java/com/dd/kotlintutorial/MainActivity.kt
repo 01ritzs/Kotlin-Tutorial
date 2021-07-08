3b1dc09fea0a49ea6b1409f8ef0d24f5c6de9b68
Ritesh Kumar Sinha <01ritzs@gmail.com>
2021-07-08 15:30:07 +0530

    Addition of few code lines

--- app/src/main/java/com/dd/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/dd/kotlintutorial/MainActivity.kt
@@ -9,5 +9,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("Hello World!")
         println("My name is Ritesh Kumar Sinha")
+
+        var ritesh: String = "Ritesh Kumar Sinha"
+        println(ritesh)
+
+        var riteshWeeklySalary: Int = 32
+        var riteshMonthlySalary: Int = 32 * 4
+        println("Ritesh weekly salary is $riteshWeeklySalary")
+        println("Ritesh monthly salary is $riteshMonthlySalary")
+
+        val apples: Int = 6
+        val oranges: Int = 5
+        var fruits = apples - oranges
+        println("Remaining fruits is $fruits")
+        println(apples / 4)
+
+        println("A quater of the apple is ${apples / 4}")
+
+        val weeks: Int = 130
+        val years: Double = weeks / 52.0
+        println("$weeks weeks is $years years ")
+
+        println("My name is $ritesh")
     }
 }
