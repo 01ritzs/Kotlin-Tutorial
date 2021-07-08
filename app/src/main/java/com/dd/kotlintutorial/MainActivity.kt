5c63231975bac8335e7035c8d2a57e5bb673aff3
Ritesh Kumar Sinha <01ritzs@gmail.com>
2021-07-08 16:43:32 +0530

    Addition of few code lines

--- app/src/main/java/com/dd/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/dd/kotlintutorial/MainActivity.kt
@@ -10,23 +10,25 @@ class MainActivity : AppCompatActivity() {
         println("Hello World!")
         println("My name is Ritesh Kumar Sinha")
 
-        var ritesh: String = "Ritesh Kumar Sinha"
+        val sinha = "Ritesh Sinha"
+        val ritesh = "Ritesh Kumar Sinha"
         println(ritesh)
 
-        var riteshWeeklySalary: Int = 32
-        var riteshMonthlySalary: Int = 32 * 4
-        println("Ritesh weekly salary is $riteshWeeklySalary")
-        println("Ritesh monthly salary is $riteshMonthlySalary")
+        val riteshWeeklySalary = 32
+        val riteshMonthlySalary = 32 * 4
+        println("$ritesh weekly salary is $riteshWeeklySalary")
+        println("$ritesh monthly salary is $riteshMonthlySalary")
 
-        val apples: Int = 6
-        val oranges: Int = 5
-        var fruits = apples - oranges
+        val apples = 6
+        val oranges = 5
+        val fruits = apples - oranges
         println("Remaining fruits is $fruits")
         println(apples / 4)
+        println("Other person name is $sinha")
 
         println("A quater of the apple is ${apples / 4}")
 
-        val weeks: Int = 130
+        val weeks = 130
         val years: Double = weeks / 52.0
         println("$weeks weeks is $years years ")
 
