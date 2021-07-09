105e6f320f5566eae5b29a0f93e872761e9caf31
Ritesh Kumar Sinha <01ritzs@gmail.com>
2021-07-09 12:21:17 +0530

    New example for kotlin tutorial

--- app/src/main/java/com/dd/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/dd/kotlintutorial/MainActivity.kt
@@ -7,31 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Hello World!")
-        println("My name is Ritesh Kumar Sinha")
 
-        val sinha = "Ritesh Sinha"
-        val ritesh = "Ritesh Kumar Sinha"
-        println(ritesh)
-
-        val riteshWeeklySalary = 32
-        val riteshMonthlySalary = 32 * 4
-        println("$ritesh weekly salary is $riteshWeeklySalary")
-        println("$ritesh monthly salary is $riteshMonthlySalary")
-
-        val apples = 6
-        val oranges = 5
-        val fruits = apples - oranges
-        println("Remaining fruits is $fruits")
-        println(apples / 4)
-        println("Other person name is $sinha")
-
-        println("A quater of the apple is ${apples / 4}")
-
-        val weeks = 130
-        val years: Double = weeks / 52.0
-        println("$weeks weeks is $years years ")
-
-        println("My name is $ritesh")
+        val lives = 3
+        if (lives < 1) {
+            println("Game Over!")
+        }
     }
 }
