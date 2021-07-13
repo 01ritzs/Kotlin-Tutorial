e08112ddc55baa874a936e9a075e60449143f555
Ritesh Kumar Sinha <01ritzs@gmail.com>
2021-07-13 11:56:54 +0530

    Addition of when and if else

--- app/src/main/java/com/dd/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/dd/kotlintutorial/MainActivity.kt
@@ -8,9 +8,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val lives = 3
-        if (lives < 1) {
-            println("Game Over!")
+//        val lives = 3
+//        if (lives < 1) {
+//            println("Game Over!")
+//        }
+
+        println("How old are you?")
+        val age = readLine()!!.toInt()
+        println("age is $age")
+
+        val message: String = when {
+            age < 18 -> "You are not eligible"
+            age == 100 -> "Congratulation"
+            else -> "You can vote"
         }
+        println(message)
     }
 }
